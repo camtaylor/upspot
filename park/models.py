d09c7be35d84ff16300e3af02448a2e7085e786a
Cameron Taylor <camerontaylor00@gmail.com>
2016-03-16 14:46:14 -0700

    Polished map and reservation screen

--- park/models.py
+++ park/models.py
@@ -14,6 +14,20 @@ class Spot(models.Model):
   num_ratings = models.IntegerField(default=0)
   num_reservations = models.IntegerField(default=0)
 
+  def shorthand_address(self):
+    """
+      Returns street address from address.
+      Ex. 841 E Meda, Glendora, CA, US ---> 841 E Meda
+    """
+    return self.address.split(",")[0]
+
+  def walking_time(self):
+    """
+      Returns an estimated walking time in minutes from a distance.
+      TODO: Add a try catch because distance is an annotated value.
+    """
+    return int(self.distance.m // 83)
+
 class Vehicle(models.Model):
   owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
   make = models.CharField(max_length=50)
@@ -36,11 +50,22 @@ class BuyerProfile(models.Model):
   num_ratings = models.IntegerField(default=0)
   num_reservations = models.IntegerField(default=0)
 
+class SellerProfile(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  name = models.CharField(max_length=35)
+  phone_number = models.CharField(max_length=20)
+  rating = models.IntegerField(default=0)
+  num_ratings = models.IntegerField(default=0)
+  num_reservations = models.IntegerField(default=0)
+  stripe_id = models.CharField(max_length=50)
+
+
 class GeoBucket(models.Model):
   geohash = models.CharField(max_length=50)
   spots = models.IntegerField(default=0)
+  reservations = models.IntegerField(default=0)
   searches = models.IntegerField(default=0)
-
+  price = models.IntegerField(default=500)
   def search(self):
     self.searches += 1
   def spot(self):
